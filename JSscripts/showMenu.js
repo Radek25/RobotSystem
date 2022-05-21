00e5eb7703dda8ec6aaaebc642ed4d4a46ea383c
My Name <66422260+Radek25@users.noreply.github.com>
2022-05-21 18:50:51 +0200

    Create RWD navbar functionality

---
+++ JSscripts/showMenu.js
@@ -0,0 +1,45 @@
+const navHeader = document.querySelector("header");
+const burgerMenuIcon = document.querySelector("#burgerMenuBtn");
+const closeMenuIcon = document.querySelector("#closeMenuBtn");
+
+let menuFlag = false;
+
+burgerMenuIcon.classList.add('showMenuIcon');
+closeMenuIcon.classList.add('hideMenuIcon');
+navHeader.classList.add('hideResponsiveLeftMenu');
+
+burgerMenuIcon.addEventListener("click", () => menuClick());
+closeMenuIcon.addEventListener("click", () => menuClick());
+
+function menuClick(){
+    menuFlag = !menuFlag;
+    if(menuFlag){
+        burgerMenuIcon.classList.remove('showMenuIcon');
+        burgerMenuIcon.classList.add('hideMenuIcon');
+        closeMenuIcon.classList.remove('hideMenuIcon');
+        closeMenuIcon.classList.add('scale-in-center');
+        closeMenuIcon.classList.add('showMenuIcon');
+    }
+    else{
+        closeMenuIcon.classList.remove('showMenuIcon');
+        closeMenuIcon.classList.add('hideMenuIcon');
+        burgerMenuIcon.classList.remove('hideMenuIcon');
+        burgerMenuIcon.classList.add('scale-in-center');
+        burgerMenuIcon.classList.add('showMenuIcon');
+    }
+    showOrHideLeftMenu(menuFlag);
+}
+
+function showOrHideLeftMenu(menuFlag){
+    if(!menuFlag){
+        navHeader.classList.remove('slide-in-left');
+        navHeader.classList.add('slide-out-left');
+        document.body.style.overflow = "auto";
+    }
+    else{
+        navHeader.classList.remove('hideResponsiveLeftMenu');
+        navHeader.classList.remove('slide-out-left');
+        navHeader.classList.add('slide-in-left');
+        document.body.style.overflow = "hidden";
+    }
+}
