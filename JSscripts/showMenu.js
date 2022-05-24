8d46a85bc37b91f18c638cfe543a354b23dc52de
My Name <66422260+Radek25@users.noreply.github.com>
2022-05-24 16:05:53 +0200

    Create base RWD for mobile

--- JSscripts/showMenu.js
+++ JSscripts/showMenu.js
@@ -14,18 +14,14 @@ closeMenuIcon.addEventListener("click", () => menuClick());
 function menuClick(){
     menuFlag = !menuFlag;
     if(menuFlag){
-        burgerMenuIcon.classList.remove('showMenuIcon');
         burgerMenuIcon.classList.add('hideMenuIcon');
         closeMenuIcon.classList.remove('hideMenuIcon');
         closeMenuIcon.classList.add('scale-in-center');
-        closeMenuIcon.classList.add('showMenuIcon');
     }
     else{
-        closeMenuIcon.classList.remove('showMenuIcon');
         closeMenuIcon.classList.add('hideMenuIcon');
         burgerMenuIcon.classList.remove('hideMenuIcon');
         burgerMenuIcon.classList.add('scale-in-center');
-        burgerMenuIcon.classList.add('showMenuIcon');
     }
     showOrHideLeftMenu(menuFlag);
 }
